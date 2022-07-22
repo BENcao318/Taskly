63b2e4545e863ea604dd21837b9849fc8f329c99
BENcao318 <labber@DESKTOP-U16DFJJ.localdomain>
2022-07-22 18:33:52 -0400

    Setup server.js and dependencies

---
+++ server/server.js
@@ -0,0 +1,26 @@
+require('dotenv').config({ path: __dirname + '/.env' })
+
+const PORT = process.env.PORT || 8080
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const app = express()
+
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
+
+//Todo
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hi' })
+})
+
+const userRouter = require('./routes/users')
+
+app.use('/users', userRouter)
+
+app.listen(PORT, () => {
+  console.log(`App listening on port: ${PORT}`)
+})
