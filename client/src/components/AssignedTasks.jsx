76b4441771995bd1bfa14e74b40ff8bf0ea769ea
Daryan Zycki <daryan@Daryans-MBP.phub.net.cable.rogers.com>
2022-08-03 22:07:18 -0400

    Add SurveyJS form reader library and add hard coded form to Client Detail page.

--- client/src/components/AssignedTasks.jsx
+++ client/src/components/AssignedTasks.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { TaskQuestion } from "../components/TaskQuestion";
 import { TaskResponse } from "../components/TaskResponse";
+import { LoadSurvey } from "./LoadSurvey";
+import { Card } from "flowbite-react";
+import { ReactComponent as Incomplete } from "../assets/incomplete.svg";
 
 export function AssignedTasks(props) {
   const { summary, phoneNumber, email } = props;
@@ -8,10 +11,18 @@ export function AssignedTasks(props) {
   return (
     <div className="w-2/4 h-full bg-gray-50 p-4">
       <p className="my-2 text-xl font-bold text-gray-900">Assigned Tasks</p>
-      <TaskQuestion
-        taskTitle={"This is an example of an outstanding task."}
-        form={"This will be a SurveyJS form conponent"}
-      />
+      <div className="mb-2">
+        <Card>
+          <div className="flex justify-start items-center">
+            <Incomplete className="h-6 mr-3 sm:h-9" alt="Complete Icon" />
+            <span className="text-lg font-medium text-gray-900 my-0">
+              Example of outstanding task
+            </span>
+          </div>
+          <LoadSurvey />
+        </Card>
+      </div>
+
       <TaskResponse
         taskTitle={"This is an example of an outstanding task."}
         form={"This will be a SurveyJS form conponent"}
