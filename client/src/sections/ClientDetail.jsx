471c01fedcdccb20db78d181f852e1390bcafec0
Daryan Zycki <daryan@Daryans-MBP.ht.home>
2022-08-12 13:47:43 -0400

    Update client info to call API instead of using API.

--- client/src/sections/ClientDetail.jsx
+++ client/src/sections/ClientDetail.jsx
@@ -1,22 +1,37 @@
-import React from 'react'
-import { ClientInfo } from '../components/ClientInfo'
-import { AssignedTasks } from '../components/AssignedTasks'
-import { TaskOverview } from '../components/TaskOverview'
-import { useContext } from 'react'
-import { clientContext } from '../context/ClientContext'
+import React, { useState, useEffect } from "react";
+import { ClientInfo } from "../components/ClientInfo";
+import { AssignedTasks } from "../components/AssignedTasks";
+import { TaskOverview } from "../components/TaskOverview";
+import { useContext } from "react";
+import { clientContext } from "../context/ClientContext";
+import serverAPI from "../hooks/useAxios";
 
 export const ClientDetail = () => {
-  const { clientUUID } = useContext(clientContext)
+  const { clientUUID } = useContext(clientContext);
+  const [client, setClient] = useState([]);
+
+  useEffect(() => {
+    serverAPI
+      .get(`/users/client-info?client_uuid=${clientUUID}`)
+      .then((response) => {
+        if (response.data.success) {
+          setClient(response.data.clientInfo);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <div className="flex w-full h-screen">
       <ClientInfo
-        summary={'This is a test'}
-        email={'yourname@example.com'}
-        phoneNumber={'222-333-4444'}
+        summary={client.summaryOfNeeds}
+        email={client.email}
+        phoneNumber={client.phoneNumber}
       />
-      <AssignedTasks />
-      <TaskOverview />
+      <AssignedTasks clientUUID={clientUUID} />
+      <TaskOverview clientUUID={clientUUID} />
     </div>
-  )
-}
+  );
+};
