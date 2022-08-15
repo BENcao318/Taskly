8936e4c90d26d2ff290ba5ae715e0ffcf6b66ab5
Daryan Zycki <daryan@Daryans-MBP.ht.home>
2022-08-15 13:36:25 -0400

    Add client view of profile page.

---
+++ client/src/components/TaskOverviewClientVarient.jsx
@@ -0,0 +1,135 @@
+import React, { useContext, useState, useRef } from "react";
+import { Transition } from "@headlessui/react";
+import { Button, Progress, Accordion } from "flowbite-react";
+import { ReactComponent as DownChevron } from "../assets/downChevron.svg";
+import { ReactComponent as PaperAirplane } from "../assets/PaperAirplane.svg";
+import { ReactComponent as PenLogo } from "../assets/penLogo.svg";
+import { ReactComponent as TrashCanLogo } from "../assets/trashcanLogo.svg";
+import { TaskList } from "../components/TaskList";
+import serverAPI from "../hooks/useAxios";
+import { toast } from "react-toastify";
+import { clientContext } from "../context/ClientContext";
+import { taskContext } from "../context/TaskContext";
+
+export function TaskOverviewClientVarient(props) {
+  const { assignedTasks, client, uuid } = props;
+  const buttonRef = useRef(null);
+  const [toggleActionMenu, setToggleActionMenu] = useState(false);
+  const { setEditClientInfo, setClientUUID } = useContext(clientContext);
+  const [openEditClientModal, setOpenEditClientModal] = useState();
+  const [openDeleteClientModal, setOpenDeleteClientModal] = useState({
+    isOpen: false,
+    uuid: "",
+  });
+  const { setEditAssignedTasks } = useContext(taskContext);
+
+  let doneTasks = 0;
+  let notDoneTasks = 0;
+
+  assignedTasks.forEach((element) => {
+    if (element.completed) {
+      doneTasks += 1;
+    } else {
+      notDoneTasks += 1;
+    }
+  });
+
+  const handleClickEditClientButton = () => {
+    serverAPI
+      .get(`/users/client-info?client_uuid=${uuid}`)
+      .then((response) => {
+        if (response.data.success) {
+          setEditClientInfo((prev) => ({
+            ...prev,
+            ...response.data.clientInfo,
+            uuid: uuid,
+          }));
+          setEditAssignedTasks((prev) => [...response.data.assignedTasks]);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    setOpenEditClientModal(true);
+  };
+
+  const sendTasksToClient = () => {
+    serverAPI
+      .post("/users/client/send-tasks", { client_email: client.email })
+      .then((response) => {
+        if (response.data.success) {
+          toast.success(`Successfully sent the tasks to the client 😊`, {
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+          });
+        } else {
+          toast.error(
+            `Error sending the tasks to the client, please try again 🙌`,
+            {
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+            }
+          );
+        }
+      })
+      .catch((err) => {
+        toast.error(
+          `Error sending the tasks to the client, please try again 🙌`,
+          {
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+          }
+        );
+        console.log(err);
+      });
+  };
+
+  return (
+    <div className="w-1/4 fixed bottom-0 left-0 bg-white">
+      <Accordion alwaysOpen={true}>
+        <Accordion.Panel>
+          <Accordion.Title>
+            Jump to a Task
+            <div className="text-base text-xs text-green-700">
+              {doneTasks}/{doneTasks + notDoneTasks} Complete
+            </div>
+            <Progress
+              progress={(doneTasks / (doneTasks + notDoneTasks)) * 100}
+              color="green"
+              size="sm"
+            />
+          </Accordion.Title>
+          <Accordion.Content>
+            {assignedTasks.map((task) => {
+              if (!task.completed) {
+                return (
+                  <TaskList
+                    key={task.task_id}
+                    complete={task.completed}
+                    taskTitle={task["task.form_json_data"]["title"]}
+                    id={task.id}
+                  />
+                );
+              }
+            })}
+            {assignedTasks.map((task) => {
+              if (task.completed) {
+                return (
+                  <TaskList
+                    key={task.task_id}
+                    complete={task.completed}
+                    taskTitle={task["task.form_json_data"]["title"]}
+                    id={task.id}
+                  />
+                );
+              }
+            })}
+          </Accordion.Content>
+        </Accordion.Panel>
+      </Accordion>
+    </div>
+  );
+}
--- client/src/pages/ClientViewPage.jsx
+++ client/src/pages/ClientViewPage.jsx
@@ -1,13 +1,106 @@
-import React from 'react'
-import { useLocation } from 'react-router-dom'
+import React, { useEffect, useState, useContext } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { AssignedTasks } from "../components/AssignedTasks";
+import { TaskOverviewClientVarient } from "../components/TaskOverviewClientVarient";
+import { clientContext } from "../context/ClientContext";
+import { taskContext } from "../context/TaskContext";
+import serverAPI from "../hooks/useAxios";
+import { ToastContainer } from "react-toastify";
+import { ReactComponent as PencilLogo } from "../assets/pencil.svg";
 
 export const ClientViewPage = () => {
-  const useQuery = () => new URLSearchParams(useLocation().search)
-  let query = useQuery()
+  const { client, setClient } = useContext(clientContext);
+  const [completedTasks, setCompletedTasks] = useState([]);
+  const { assignedTasks, setAssignedTasks } = useContext(taskContext);
+  const { uuid } = useParams();
+  const navigate = useNavigate();
 
-  return (
-    <div>
-      <h1>ClientUUID {query.get('client_uuid')}</h1>
-    </div>
-  )
-}
+  useEffect(() => {
+    serverAPI
+      .get(`/users/client-info?client_uuid=${uuid}`)
+      .then((response) => {
+        if (response.data.success) {
+          setClient(response.data.clientInfo);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    serverAPI
+      .get(`/tasks/assigned?client_uuid=${uuid}`)
+      .then((response) => {
+        if (response.data.success) {
+          setAssignedTasks(response.data.assignedTasks);
+          setCompletedTasks(response.data.completedTasks);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [uuid]);
+
+  if (client && assignedTasks) {
+    return (
+      <div>
+        <nav className="bg-white border-b border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+          <div className="flex flex-wrap items-center justify-between">
+            <div className="flex items-center ml-2">
+              <div
+                className="flex items-center cursor-pointer"
+                onClick={() => navigate("/home")}
+              >
+                <PencilLogo />
+                <span className="self-center px-2 text-xl font-semibold whitespace-nowrap dark:text-white">
+                  Taskly
+                </span>
+              </div>
+            </div>
+          </div>
+        </nav>
+        <div className="flex-col w-full h-screen p-20 pt-0">
+          <div className="border-b">
+            <p className="mt-6 ml-4 text-2xl font-semibold text-gray-900">
+              {client.firstName} {client.lastName}
+            </p>
+            <p className="ml-4 mb-4 text-base font-normal text-gray-500 text">
+              {client.email} | {client.phoneNumber}
+            </p>
+            <p className="ml-4 text-base font-bold text-gray-900">
+              Summary of Needs
+            </p>
+            <p className="ml-4 mb-4 text-base font-normal text-gray-500 text">
+              {client.summaryOfNeeds}
+            </p>
+          </div>
+          <div className="flex flex-col">
+            <AssignedTasks
+              client={client}
+              assignedTasks={assignedTasks}
+              completedTasks={completedTasks}
+            />
+            <TaskOverviewClientVarient
+              assignedTasks={assignedTasks}
+              client={client}
+              completedTasks={completedTasks}
+              uuid={uuid}
+            />
+          </div>
+          <ToastContainer
+            toastClassName={() =>
+              "relative flex px-2 py-4 min-h-16 rounded-md justify-between overflow-hidden cursor-pointer bg-sky-200 text-black font-semibold"
+            }
+            position="top-center"
+            autoClose={6000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+          />
+        </div>
+      </div>
+    );
+  }
+};
